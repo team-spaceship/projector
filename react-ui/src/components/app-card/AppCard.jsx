b18d294e8fa33fe7e507b66951595cc1eb63fb2f
Tim van der Hoek <timvanderhoek1994@gmail.com>
2017-11-20 16:53:04 +0100

    Visual update for projector appcards.

--- react-ui/src/components/app-card/AppCard.jsx
+++ react-ui/src/components/app-card/AppCard.jsx
@@ -4,18 +4,34 @@ import PropTypes from 'prop-types';
 class AppCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    console.log('this is:', this);
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
   
   render() {
     const { app } = this.props;
+
     return (
       <div key={app._id} className="app--card">
         <div className="app--card-header">
-          <h3>{app.name}</h3>
+          {/* Hier moet een application image gevuld worden. Deze is er volgens mij nog niet? Of is dit app_icon of app_banner? */}
+          <img className="app--card-image" src="https://picsum.photos/280/200/?random" alt="app-logo"/>
+          <p className="app--card-description">{app.description}</p>
         </div>
         <div className="app--card-body">
-          <p>{app.description}</p>
+          <h4>{app.name}</h4>
+        {/* Is er een application type/category of iets in die richting? Die kan dan hier ingevuld worden. */}
+          <span className="app--card-type">Application Type</span>
+          <a className={"button " + (this.state.isToggleOn ? 'pink' : 'blue')} href="#" onClick={this.handleClick}>
+            {this.state.isToggleOn ? 'Install' : 'Installing'}
+          </a>
         </div>
       </div>
     );
