9de53cba24f1079f3c20996ac915a9045c9ca43d
Tim van de Vathorst <tim@vandevathorst.nl>
2017-12-18 14:06:35 +0100

    Add: gitignore for apps folder. And did some more cleanup.. again

--- src/websocket-server.js
+++ src/websocket-server.js
@@ -6,17 +6,9 @@ export default class WebsocketServer {
   static create(io) {
     const sockets = [];
     io.sockets.on('connection', (socket) => {
-      console.log('Connection! Socket ID: ' + socket.id);
       sockets.push(socket);
       
       socket.on('setActiveApp', (data) => {
-        console.log("Setting active app to: " + data.app);
-      
-        // // Trigger renderApp event.
-        // for (let i = 0; i < sockets.length; i += 1) {
-        //   sockets[i].emit('renderApp', { app: data.app });
-        // }
-
         io.sockets.emit('renderApp', { app: data.app });
       });
     });
